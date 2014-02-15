eb3d540abaf9eeb30c0de800252251755a25336a
Kengsreng Tang <kengsrengkh@gmail.com>
2014-02-14 21:04:19 -0800

    specify foreign key

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :course, :primary_key => 'course_id'
-  belongs_to :student, :primary_key => 'user_id'
+  belongs_to :student, :primary_key => 'user_id', :foreign_key => 'user_id'
+
 end
