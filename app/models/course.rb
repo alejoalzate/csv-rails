7a2acdb6f9448c68b0332a6455b682415eebcae8
Kengsreng Tang <kengsrengkh@gmail.com>
2014-02-14 21:33:19 -0800

    list all active courses and students

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ActiveRecord::Base
   has_many :enrollments, :primary_key => 'course_id'
   has_many :students, through: :enrollments, :primary_key => 'course_id'
+
+  def get_active_students
+    students.where('enrollments.state' => 'active', 'students.state' => 'active')
+  end
 end
