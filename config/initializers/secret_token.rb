d3a560bdcf4f51974ac9e6ba17b9995faa5743df
Kengsreng Tang <kengsrengkh@gmail.com>
2014-02-14 19:32:47 -0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CsvRails::Application.config.secret_key_base = '7e075c07d50d81dbc927064ed730a6ad4304b040e25d16df3043c93be96733b67b4cefd5c5b57966301e9e5f1958b7e25f839675b77eee79eeb2ee147cdc81c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CsvRails::Application.config.session_store :cookie_store, key: '_csv-rails_session'
